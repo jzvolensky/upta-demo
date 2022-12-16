aadd86607adc16f4e15d6b0e2ff0ce07fbacffe5
Juraj Zvolensky <juro.zvolensky@gmail.com>
2022-12-16 20:25:33 +0100

    Initial commit

---
+++ pages/about/about.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '../../styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>About UPTA</title>
+        <meta name="about"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <main className={styles.main}>
+          <div className={styles.description}>
+            <h1 className={styles.headings}>
+                About UPTA
+            </h1>
+
+          
+          </div>
+        </main>
+    </>
+  )
+}
---
+++ pages/changelog/changelog.tsx
@@ -0,0 +1,48 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '../../styles/Home.module.css'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const inter = Inter({ subsets: ['latin'] })
+
+
+
+
+export default function Changelog() {
+  return (
+    <>
+      <Head>
+        <title>Changelog</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <h1 className={styles.headings}>
+            Changelog:
+          </h1>
+          <Image
+            src="/../public/Gear_icon_changelog.png"
+            alt="Changelog gear"
+            className={styles.logo}
+            width={130}
+            height={125}
+            priority
+            />
+        </div>
+        <div className={styles.list_parent}>
+          <div className={styles.changelog_entry}>
+            <h2>Version 0.0.1 released: 16/12/2022</h2>
+            <h3>General changes:</h3>
+            <ul className={styles.changelog_list}> 
+              <li> Initial release of the platform</li>
+              <li> Creation of supporting subpages (About, Github, Solid, and Changelog)</li>
+              <li> Added placeholder buttons for login functionality</li>  
+            </ul>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
